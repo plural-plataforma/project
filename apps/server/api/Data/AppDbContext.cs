3d8848f323477bedac670d0f8f6907a47b3cb433
Carla Reis <47834995+carla-reis-cr@users.noreply.github.com>
2025-09-15 23:54:03 -0300

    clear project

--- apps/server/api/Data/AppDbContext.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace PluralPlataforma.Api.Data;
-
-public class AppDbContext : DbContext
-{
- public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-}
