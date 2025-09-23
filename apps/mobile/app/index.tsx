174b1aeba51ecf96056bab88d5e1c38f025b1f81
Carla Reis <47834995+carla-reis-cr@users.noreply.github.com>
2025-09-22 23:08:37 -0300

    feat: using react-native-paper ui [PLUR-14]

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,15 +1,44 @@
-import { Button, Text, View } from 'react-native'
+import * as React from 'react'
+import { BottomNavigation, Text } from 'react-native-paper'
+
+const MusicRoute = () => <Text>Music</Text>
+
+const AlbumsRoute = () => <Text>Albums</Text>
+
+const RecentsRoute = () => <Text>Recents</Text>
+
+const NotificationsRoute = () => <Text>Notifications</Text>
 
 export default function Index() {
+  const [index, setIndex] = React.useState(0)
+  const [routes] = React.useState([
+    {
+      key: 'music',
+      title: 'Favorites',
+      focusedIcon: 'heart',
+      unfocusedIcon: 'heart-outline'
+    },
+    { key: 'albums', title: 'Albums', focusedIcon: 'album' },
+    { key: 'recents', title: 'Recents', focusedIcon: 'history' },
+    {
+      key: 'notifications',
+      title: 'Notifications',
+      focusedIcon: 'bell',
+      unfocusedIcon: 'bell-outline'
+    }
+  ])
+
+  const renderScene = BottomNavigation.SceneMap({
+    music: MusicRoute,
+    albums: AlbumsRoute,
+    recents: RecentsRoute,
+    notifications: NotificationsRoute
+  })
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}
-    >
-      <Button title="Hello, world!" onPress={() => alert('Button pressed!')} />
-    </View>
+    <BottomNavigation
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    />
   )
 }
