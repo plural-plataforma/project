d65ed836f59f33c367245c4b341748ec65207a8c
carla-reis-cr <carla.cr.reis@gmail.com>
2025-10-18 16:05:06 -0300

    feat: criação da tela de cadastro/edição [PLUR-16]

--- apps/mobile/src/app/planejamento/PlanejamentoScreen.tsx
+++ apps/mobile/src/app/planejamento/PlanejamentoScreen.tsx
@@ -1,26 +1,71 @@
 import { colors } from '@/packages/ui/theme/theme'
-import { Text, View, StyleSheet} from 'react-native'
+import Header from '@src/components/Header'
+import InputField from '@src/components/InputField'
+import { Planejamento } from '@src/types/planejamento'
+import { useLocalSearchParams, useRouter } from 'expo-router'
+import { useMemo } from 'react'
+import { Text, View, StyleSheet, FlatList } from 'react-native'
+
 
 export default function PlanejamentoScreen() {
+  const router = useRouter()
+  const params = useLocalSearchParams();
+  const planejamentoId = params.id ? Number(params.id) : null;
+  const isEdit = !!planejamentoId;
+
+  const formFields = useMemo(() =>[
+    {
+      id: 'apelido',
+      label: "Nome do PDI",
+      placeholder: "",
+      value:  '',
+    },
+    {
+      id: 'etapaEnsino',
+      label: "Etapa de Ensino",
+      placeholder: "",
+      value:  '',
+    },
+    {
+      id: 'tipoHabilidade',
+      label: "Tipo de Habilidade",
+      placeholder: "",
+      value:  '', 
+    }
+  ], []);
+
+  
+  
   return (
 
     <View style={styles.container}>
-      <Text style={styles.text}>Em breve disponivel</Text>
+      <Header title={isEdit ? "Editar PDI" : "Cadastro de PDI"} onBack={() => router.back()} fixed={true} />
+      <FlatList data={formFields}
+        renderItem={({ item, index }) => (
+          <View style={[{marginBottom: 15}, { zIndex: formFields.length - index }]}>
+            <InputField {...item} />
+            
+          </View>
+        )}
+         keyExtractor={(item) => item.id.toString()}
+      />
     </View>
   )
 }
 
-export  const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
-    flex: 1, 
     backgroundColor: colors.background,
-    alignItems: 'center', 
-    justifyContent: 'center'
-  },  
+    paddingHorizontal: 20,
+    paddingTop: 70
+  },
+  content: {
+    paddingBottom: 20,
+  },
   text: {
     fontSize: 20,
     fontWeight: '700',
-    color: colors.primary,  
+    color: colors.primary,
     fontFamily: 'Nunito_700Bold',
     paddingInlineStart: 10,
   },
