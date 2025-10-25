13a799e0c640be8cedfde429ecddf64d8720b672
juliavdev <juliav.dev@gmail.com>
2025-10-25 10:27:02 -0300

    adicionaPlanejamentoBuscarAlunos

---
+++ apps/api/DTOs/Planejamento/PlanejamentoBuscarSimplificadoDTO.cs
@@ -0,0 +1,19 @@
+﻿using api.DTOs.Aluno;
+using api.DTOs.Habilidade;
+
+namespace api.DTOs.Planejamento
+{
+    public class PlanejamentoBuscarSimplificadoDTO
+    {
+        public int Id { get; set; }
+
+        public string Apelido { get; set; }
+
+        public DateOnly DataInicio { get; set; }
+
+        public DateOnly DataFim { get; set; }
+
+        public List<HabilidadeBuscarDTO> Habilidades { get; set; } = new();
+
+    }
+}
