625933bad08e378b95a1f99b9cf093ab76a5015f
carla-reis-cr <carla.cr.reis@gmail.com>
2025-12-01 22:47:09 -0300

    feat: criação da tabela e endpoints de avaliações [PLUR-45]

--- apps/api/Data/AppDbContext.cs
+++ apps/api/Data/AppDbContext.cs
@@ -18,10 +18,13 @@ namespace Data
         public DbSet<Planejamento> Planejamentos { get; set; }
         public DbSet<AlunosXPlanejamento> AlunosXPlanejamentos { get; set; }
         public DbSet<HabilidadesXPlanejamento> HabilidadesXPlanejamentos { get; set; }
+
         public DbSet<Laudo> Laudos { get; set; }
 
         public DbSet<Estrategias> Estrategias { get; set; }
         public DbSet<EstrategiasXPlanejamento> EstrategiasXPlanejamentos { get; set; }
+        public DbSet<Avaliacao> Avaliacao { get; set; }
+        public DbSet<AvaliacaoXPlanejamento> AvaliacaoXPlanejamento { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
@@ -100,6 +103,21 @@ namespace Data
                 .HasOne(ph => ph.Estrategia)
                 .WithMany(h => h.EstrategiasXPlanejamentos)
                 .HasForeignKey(ph => ph.EstrategiaId);
+
+
+            // Planejamento ↔ Avaliacao (N:N)
+            modelBuilder.Entity<AvaliacaoXPlanejamento>()
+                .HasKey(ph => new { ph.PlanejamentoId, ph.AvaliacaoId });
+
+            modelBuilder.Entity<AvaliacaoXPlanejamento>()
+                .HasOne(ph => ph.Avaliacao)
+                .WithMany(p => p.AvaliacaoXPlanejamento)
+                .HasForeignKey(ph => ph.PlanejamentoId);
+
+            modelBuilder.Entity<AvaliacaoXPlanejamento>()
+                .HasOne(ph => ph.Avaliacao)
+                .WithMany(h => h.AvaliacaoXPlanejamento)
+                .HasForeignKey(ph => ph.AvaliacaoId);
 
             // Planejamento ↔ Aluno (N:N)
             modelBuilder.Entity<AlunosXPlanejamento>()
