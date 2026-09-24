e04edf84b4cad40e8d07f3e0454a9ba7e401a400
Leonardo Rodrigues <leorodriguesoffice@gmail.com>
2026-09-24 17:24:26 -0300

    Add IdProfessor foreign key to Habilidades table
    - Introduced a new column 'idprofessor' in the 'habilidades' table to establish a relationship with the 'professores' table.
    - Created an index on 'idprofessor' for optimized queries.
    - Added foreign key constraint to enforce referential integrity between 'habilidades' and 'professores'.

---
+++ apps/api/Migrations/20260924202339_HabilidadeIdProfessor.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class HabilidadeIdProfessor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "idprofessor",
+                table: "habilidades",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_habilidades_idprofessor",
+                table: "habilidades",
+                column: "idprofessor");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_habilidades_professores_idprofessor",
+                table: "habilidades",
+                column: "idprofessor",
+                principalTable: "professores",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_habilidades_professores_idprofessor",
+                table: "habilidades");
+
+            migrationBuilder.DropIndex(
+                name: "ix_habilidades_idprofessor",
+                table: "habilidades");
+
+            migrationBuilder.DropColumn(
+                name: "idprofessor",
+                table: "habilidades");
+        }
+    }
+}
